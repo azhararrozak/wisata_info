90742ed7c0ddd8f9cbc14b5d0619b4b08de790e8
azhararrozak <azhardrozak2001@gmail.com>
2022-01-02 18:18:08 +0700

    Create about_contact.blade.php

---
+++ resources/views/template_infowisata/about_contact.blade.php
@@ -0,0 +1,83 @@
+<!--Start About -->
+    <section id="About" class="p-5">
+      <div class="container">
+        <div class="title">
+          <h1>About</h1>
+        </div>
+        <div class="row align-items-center justify-content-between">
+          <div class="col-md">
+            <img src="assets/img/bromo.jpg" class="img-fluid" alt="" />
+          </div>
+          <div class="col-md p-5">
+            <h2>Gunung Bromo</h2>
+            <p class="lead">
+              Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut
+              dan berada dalam empat wilayah kabupaten, yakni Kabupaten
+              Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten
+              Malang.
+            </p>
+            <p>
+              Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut
+              dan berada dalam empat wilayah kabupaten, yakni Kabupaten
+              Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten
+              Malang.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="learn" class="p-5 bg-dark text-light">
+      <div class="container">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-md p-5">
+            <h2>Gunung Bromo</h2>
+            <p class="lead">
+              Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut
+              dan berada dalam empat wilayah kabupaten, yakni Kabupaten
+              Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten
+              Malang.
+            </p>
+            <p>
+              Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut
+              dan berada dalam empat wilayah kabupaten, yakni Kabupaten
+              Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten
+              Malang.
+            </p>
+          </div>
+          <div class="col-md">
+            <img src="assets/img/bromo.jpg" class="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Halaman Contact Start-->
+    <section id="Contact">
+        <div class="container mt-5">
+            <h1>Contact</h1>
+            <form class="row g-3">
+              <div class="col-md-8">
+                <label for="namadepan" class="form-label">Nama Depan</label>
+                <input type="text" class="form-control" id="namadepan" required>
+              </div>
+              <div class="col-md-8">
+                <label for="alamatInfo" class="form-label">Alamat Lengkap</label>
+                <input type="text" class="form-control" id="alamatInfo" required>
+              </div>
+              <div class="col-md-8">
+                <label for="emailInfo" class="form-label">E-mail</label>
+                <input type="email" class="form-control" id="emailInfo" required>
+              </div>
+              <div class="col-md-8">
+                <label for="nohp" class="form-label">Nomer Hp</label>
+                <input type="text" class="form-control" id="nohp" placeholder="+62 xxx xxx xxx">
+              </div>
+              <div class="col-md-12">
+                <label for="pesan" class="form-label">Pesan Yang Akan Disampaikan</label>
+                <textarea class="form-control" id="pesan" rows="3" required></textarea>
+              </div>
+              <div class="col-md-12">
+                <button type="kirim" class="btn btn-primary">kirim</button>
+              </div>
+            </form>
+        </div><br><br>
