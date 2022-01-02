48f0d348aa35027173471c9deb2facf548704892
azhararrozak <azhardrozak2001@gmail.com>
2022-01-02 18:17:40 +0700

    Create edit.blade.php

---
+++ resources/views/Dashboard_Admin/tags/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('template_admin.home_das')
+@section('sub-judul', 'Edit Tags')
+@section('content')
+
+	@if(count($errors)>0)
+		<div class="alert alert-danger" role="alert">
+			@foreach($errors->all() as $error)
+				{{ $error }}
+			@endforeach
+		</div>	
+	@endif
+	@if(Session::has('success'))
+		<div class="alert alert-success" role="alert">
+			{{ Session('success') }}
+		</div>
+	@endif
+	<form action="{{ route('tag.update', $tag->id) }}" method="post">
+		@csrf
+		@method('patch')
+		<div class="mb-3">
+		  <label>Masukkan Tag:</label>
+		  <input type="text" class="form-control" name="name" value="{{ $tag->name }}">
+		</div>
+		<div class="mb-3 d-grid gap-2">
+		  <button class="btn btn-primary">Simpan Tags</button>
+		</div>
+	</form>
+@endsection
