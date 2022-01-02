be198a39a89446cd4d1c0b69f037bad546dc8dc5
azhararrozak <azhardrozak2001@gmail.com>
2022-01-02 18:18:18 +0700

    Create head.blade.php

---
+++ resources/views/template_infowisata/head.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Wisataku</title>
+    <!-- Boostrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <!-- Tambahan CSS -->
+    <link rel="stylesheet" href="/css/custom.css" />
+    <!-- Fonts Google -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div class="container">
+        <a href="#Home" class="navbar-brand text-white fw-bold">WisataKu.com</a>
+        <button
+          type="button"
+          class="navbar-toggler"
+          data-bs-target="#navbarNav"
+          data-bs-toggle="collapse"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle Navbar"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="#Home" class="nav-link text-white">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a href="#Wisata" class="nav-link text-white">WISATA</a>
+            </li>
+            <li class="nav-item">
+              <a href="#About" class="nav-link text-white">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a href="#Contact" class="nav-link text-white">CONTACT</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ route('login') }}" class="nav-link text-white">LOGIN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
